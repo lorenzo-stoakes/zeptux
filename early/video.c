9e2bd24377fa5eb6025ff654df24ac8e8a7fd5c5
Lorenzo Stoakes <lstoakes@gmail.com>
2021-08-12 08:38:51 +0100

    early: add comments to early video code

--- early/video.c
+++ early/video.c
@@ -7,6 +7,8 @@
 
 static int curr_row = 0, curr_col = 0;
 
+// Retrieve a pointer to the video buffer which, as this is called before any
+// allocator is online, is an arbitrarily assigned area of memory.
 static char *get_buf(void)
 {
 	char *buf = (char *)EARLY_VIDEO_BUFFER_ADDRESS;
@@ -15,6 +17,9 @@ static char *get_buf(void)
 	return &buf[index];
 }
 
+// Output a character to the video buffer (but not screen, this happens on
+// newline) if the line isn't yet truncated, if it is then output nothing except
+// a '.' in the last column.
 static void putc(char chr)
 {
 	// We truncate at the end of the line.
@@ -33,6 +38,7 @@ exit:
 	curr_col++;
 }
 
+// Scroll the memory buffer down one line.
 static void scroll_down(void)
 {
 	// Copy from 2nd row to top of buffer, including hidden row to scroll
@@ -44,6 +50,7 @@ static void scroll_down(void)
 	memset(&target[MEMORY_SIZE], 0, 2 * COLS);
 }
 
+// Flush the video buffer to video memory, outputting the results to screen.
 static void flush(void)
 {
 	volatile char *tgt = (volatile char *)EARLY_VIDEO_MEMORY_ADDRESS;
@@ -54,6 +61,8 @@ static void flush(void)
 	}
 }
 
+// Handle a newline - we're line-buffered so this flushes to video memory. This
+// also scrolls if necessary.
 static void newline(void)
 {
 	if (curr_row == ROWS)
@@ -66,6 +75,7 @@ static void newline(void)
 	flush();
 }
 
+// Simulate tab output.
 static void puttab(void)
 {
 	for (int i = 0; i < 8; i++) {
@@ -73,12 +83,15 @@ static void puttab(void)
 	}
 }
 
+// Clear the area of memory we have assigned to the video buffer which we will
+// be copying to video memory.
 void early_video_init(void)
 {
 	char *buf = (char *)EARLY_VIDEO_BUFFER_ADDRESS;
 
 	memset(buf, 0, BUFFER_SIZE);
 }
+
 // Put string to video buffer as best we can.
 void early_video_puts(const char *str)
 {
