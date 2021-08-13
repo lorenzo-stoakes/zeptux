b30715d5daabcd532660e9651ea690d28e9cf22e
Lorenzo Stoakes <lstoakes@gmail.com>
2021-08-13 23:28:28 +0100

    spinlock: add initial implementation
    The fundamentals of a spinlock are busy-waiting on a atomic exchange.
    This refined version is inspired by the excellent, referenced article
    and uses compiler intrinsic atomic operations with memory model specified
    and CPU hinting to improve performance.
    
    We add this now as a prerequisite to the physical allocator implementation.
    While we have not yet implemented anything SMP, the physical allocator will
    absolutely need to acquire locks so having them ready to go from now is the
    right way to go.
    
    The userland testing framework can be used to test them using std::thread's
    and do things that would be very difficult with this extremely early stage
    kernel.

---
+++ arch/x86_64/include/spinlock.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "asm.h"
+#include "compiler.h"
+#include "types.h"
+
+// Implementation heavily inspired by https://rigtorp.se/spinlock/
+
+// Represents a spinlock object. Use a struct wrapper as a cheap form of C type
+// safety.
+typedef struct {
+	uint64_t x;
+} spinlock_t;
+
+// Simple mechanism for assigning an empty unlocked spinlock.
+static inline spinlock_t empty_spinlock(void)
+{
+	return (spinlock_t){0};
+}
+
+static inline void spinlock_acquire(spinlock_t *lock)
+{
+	while (true) {
+		// Attempt to acquire the lock right away. If the lock is
+		// available from the start we acquire it right away.
+		if (!atomic_exchange_acquire(&lock->x, 1))
+			return;
+
+		// Loop on _reads_ of the atomic value. Cache lines can be
+		// shared between cores on read, a write requires an update via
+		// the cache coherent mechanism.
+		while (atomic_load_relaxed(&lock->x)) {
+			hint_spinwait();
+		}
+	}
+}
+
+static inline void spinlock_release(spinlock_t *lock)
+{
+	atomic_store_release(&lock->x, 0);
+}
--- include/zeptux_early.h
+++ include/zeptux_early.h
@@ -16,6 +16,7 @@
 #include "elf.h"
 #include "macros.h"
 #include "mem.h"
+#include "spinlock.h"
 #include "string.h"
 #include "types.h"
 #include "ver.h"
