e0d3e0cc54164edff813f4ac4b985a1f08f18720
Lorenzo Stoakes <lstoakes@gmail.com>
2021-08-16 23:21:17 +0100

    early: mm: sort and deduplicate e820 entries

--- early/mem.c
+++ early/mem.c
@@ -8,13 +8,87 @@ static void drop_direct0(void)
 	global_flush_tlb();
 }
 
+static bool less(struct e820_entry *a, struct e820_entry *b)
+{
+	return a->base < b->base || (a->base == b->base && a->size < b->size);
+}
+
+static void sort_e820(void)
+{
+	struct early_boot_info *info = boot_info();
+
+	// Insertion sort.
+	for (int i = 1; i < (int)info->num_e820_entries; i++) {
+		struct e820_entry key = info->e820_entries[i];
+
+		int j;
+		for (j = i - 1; j >= 0 && !less(&info->e820_entries[j], &key);
+		     j--) {
+			info->e820_entries[j + 1] = info->e820_entries[j];
+		}
+		info->e820_entries[j + 1] = key;
+	}
+}
+
+static void merge_e820(void)
+{
+	struct early_boot_info *info = boot_info();
+
+	// Note that we assume that all zero-size entries have been pruned out
+	// by the boot code.
+
+	int curr_index = 0;
+	for (int i = 1; i < (int)info->num_e820_entries; i++) {
+		struct e820_entry *curr = &info->e820_entries[curr_index];
+		struct e820_entry *entry = &info->e820_entries[i];
+		range_pair_t curr_range = {
+			.from = curr->base,
+			.to = curr->base + curr->size - 1,
+		};
+		range_pair_t entry_range = {
+			.from = entry->base,
+			.to = entry->base + entry->size - 1,
+		};
+
+		if (entry->type != curr->type)
+			goto keep;
+
+		// We've sorted the arrays so we know which order the ranges are
+		// in.
+		switch (range_compare(curr_range, entry_range)) {
+		case RANGE_SEPARATE:
+			// ....
+			//      ~ ####
+			break;
+		case RANGE_ADJACENT:
+		case RANGE_OVERLAP:
+			// ....        ....
+			//     #### or   ####
+			curr->size += entry_range.to - entry_range.from + 1;
+			goto loop;
+		case RANGE_COINCIDE:
+			// ......
+			//  ####
+			goto loop;
+		}
+
+	keep:
+		curr_index++;
+		if (curr_index != i)
+			info->e820_entries[curr_index] = *entry;
+
+	loop:
+	}
+
+	info->num_e820_entries = curr_index + 1;
+}
+
 void early_meminit(void)
 {
 	drop_direct0();
+	sort_e820();
+	merge_e820();
 
-	// 1. Sort, dedupe e820
-	// 2. Determine avialable free memory
+	// 2. Determine available free memory
 	// 3. Initialise physical memblock
-
-	// Do some stuff yeah?
 }
