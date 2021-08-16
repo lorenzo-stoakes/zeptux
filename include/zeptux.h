b119bb820893ecc767b962cf90a21232c8eb4b55
Lorenzo Stoakes <lstoakes@gmail.com>
2021-08-16 19:17:48 +0100

    build: split zeptux.h out from early_zeptux.h

---
+++ include/zeptux.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "asm.h"
+#include "elf.h"
+#include "macros.h"
+#include "mem.h"
+#include "mm/phys.h"
+#include "spinlock.h"
+#include "string.h"
+#include "types.h"
+#include "ver.h"
--- include/zeptux_early.h
+++ include/zeptux_early.h
@@ -12,12 +12,4 @@
 #include "early_serial.h"
 #include "early_video.h"
 
-#include "asm.h"
-#include "elf.h"
-#include "macros.h"
-#include "mem.h"
-#include "mm/phys.h"
-#include "spinlock.h"
-#include "string.h"
-#include "types.h"
-#include "ver.h"
+#include "zeptux.h"
