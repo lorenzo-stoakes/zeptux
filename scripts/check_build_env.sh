182a5251e452de7afda7384f7623edf98ea41d18
Lorenzo Stoakes <lstoakes@gmail.com>
2021-08-23 22:19:11 +0100

    build: update gcc requirement to 11
    It turns out we rely on some newer gcc features (specifically around
    label placement, so to avoid having to juggle support of multiple gcc
    versions, simply insist on 11.

--- scripts/check_build_env.sh
+++ scripts/check_build_env.sh
@@ -2,7 +2,7 @@
 set -e; set -o pipefail
 
 gcc_maj=$(gcc -v 2>&1 | grep 'gcc version' | awk '{print $3}' | awk -F '.' '{print $1}')
-if [[ ${gcc_maj} -lt 9 ]]; then
-	echo "GCC major version is ${gcc_maj}, minimum required is 9."
+if [[ ${gcc_maj} -lt 11 ]]; then
+	echo "GCC major version is ${gcc_maj}, minimum required is 11."
 	exit 1
 fi
