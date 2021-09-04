e1b62ec95fa123ee5a2c6b11fd6ca16c7d14dc34
Lorenzo Stoakes <lstoakes@gmail.com>
2021-09-04 12:00:27 +0100

    build: check go version >= 1.16

--- scripts/check_build_env.sh
+++ scripts/check_build_env.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 set -e; set -o pipefail
 
-gcc_maj=$(gcc -v 2>&1 | grep 'gcc version' | awk '{print $3}' | awk -F '.' '{print $1}')
+function get_maj_ver()
+{
+	awk -F '.' '{print $1}'
+}
+
+function get_min_ver()
+{
+	awk -F '.' '{print $2}'
+}
+
+# Check go >= 1.16
+go_ver=$(go version | awk '{print $3}' | sed 's/go//')
+go_maj=$(echo $go_ver | get_maj_ver)
+if [[ ${go_maj} -lt 1 ]]; then
+	echo "go major version is ${go_maj}, minimum required is 1."
+	exit 1
+fi
+go_min=$(echo $go_ver | get_min_ver)
+if [[ ${go_min} -lt 16 ]]; then
+	echo "go minor version is ${go_min}, minimum required is 16."
+	exit 1
+fi
+
+# Check gcc >= 11
+gcc_maj=$(gcc -v 2>&1 | grep 'gcc version' | awk '{print $3}' | get_maj_ver)
 if [[ ${gcc_maj} -lt 11 ]]; then
 	echo "GCC major version is ${gcc_maj}, minimum required is 11."
 	exit 1
