35501621ca7a15516610ba9b057ed063047664e0
Lorenzo Stoakes <lstoakes@gmail.com>
2021-09-11 11:54:52 +0100

    test: early: add non-pagetable page tests

--- test/early/test_page_early.c
+++ test/early/test_page_early.c
@@ -1,6 +1,6 @@
 #include "test_early.h"
 
-const char *assert_correct_virtaddr(void)
+static const char *assert_correct_virtaddr(void)
 {
 	uint64_t offset = 0;
 
@@ -27,21 +27,104 @@ const char *assert_correct_virtaddr(void)
 	assert(virt_pude_index(va) == 0xad, "pude index != 0xad");
 	assert(virt_pgde_index(va) == 0xde, "pgde index != 0xde");
 
+	physaddr_t pa = {0xdeadbeef};
+	va = phys_to_virt(pa);
+	assert(va.x == KERNEL_DIRECT_MAP_BASE + 0xdeadbeef,
+	       "phys_to_virt() not giving correct direct map address");
+
+	void *ptr = phys_to_virt_ptr(pa);
+	assert((uint64_t)ptr == KERNEL_DIRECT_MAP_BASE + 0xdeadbeef,
+	       "phys_to_virt_ptr() not giving correct direct map address");
+
+	assert(virt_to_phys(va).x == 0xdeadbeef,
+	       "Converting virtual address back to physical failed");
+
+	// We also need to test that we can convert a virtual address from the
+	// kernel ELF to a physical address correctly.
+	va.x = KERNEL_ELF_ADDRESS + 0xbeef;
+	assert(virt_to_phys(va).x == 0xbeef,
+	       "virt_to_phys() not decoded ELF VA well");
+
+	ptr = (void *)va.x;
+	assert(virt_ptr_to_phys(ptr).x == 0xbeef,
+	       "virt_ptr_to_phys() not decoded ELF VA well");
+
+	return NULL;
+}
+
+static const char *assert_page_misc(void)
+{
+	assert(bytes_to_pages(0) == 0, "bytes_to_pages(0) != 0");
+	assert(bytes_to_pages(1) == 1,
+	       "1 byte requires at least 1 page to store");
+	assert(bytes_to_pages(PAGE_SIZE) == 1,
+	       "PAGE_SIZE byte requires at least 1 page to store");
+	assert(bytes_to_pages(PAGE_SIZE + 1) == 2,
+	       "PAGE_SIZE + 1 byte requires at least 2 pages to store");
+
+	physaddr_t pa = early_page_alloc();
+
+	uint8_t *ptr = phys_to_virt_ptr(pa);
+	for (int i = 0; i < (int)PAGE_SIZE; i++) {
+		ptr[i] = 123;
+	}
+	zero_page(pa);
+
+	for (int i = 0; i < (int)PAGE_SIZE; i++) {
+		assert(ptr[i] == 0, "Page not zeroed");
+	}
+
+	early_page_free(pa);
+
+	pa.x = 0xabcd;
+	assert(pa_next_page(pa).x == 0xb000,
+	       "Page next not advancing to next page");
+	pa.x = 0xafff;
+	assert(pa_next_page(pa).x == 0xb000,
+	       "Page next not advancing to next page");
+	pa.x = 0xa000;
+	assert(pa_next_page(pa).x == 0xb000,
+	       "Page next not advancing to next page");
+
+	pa.x = 0xabcd;
+	assert(pa_prev_page(pa).x == 0x9000,
+	       "Page prev not advancing to previous page");
+	pa.x = 0xafff;
+	assert(pa_prev_page(pa).x == 0x9000,
+	       "Page prev not advancing to previous page");
+	pa.x = 0xa000;
+	assert(pa_prev_page(pa).x == 0x9000,
+	       "Page prev not advancing to previous page");
+
+	pa.x = 0xabcd;
+	assert(pa_to_pfn(pa).x == 0xa,
+	       "pa_to_pfn() not correctly counting PA index");
+	pa.x = 0xa000;
+	assert(pa_to_pfn(pa).x == 0xa,
+	       "pa_to_pfn() not correctly counting PA index");
+	pa.x = 0xafff;
+	assert(pa_to_pfn(pa).x == 0xa,
+	       "pa_to_pfn() not correctly counting PA index");
+	pa.x = 0xb123;
+	assert(pa_to_pfn(pa).x == 0xb,
+	       "pa_to_pfn() not correctly counting PA index");
+
+	pfn_t pfn = {0xa};
+	assert(pfn_to_pa(pfn).x == 0xa000,
+	       "pfn_to_pa() not correctly converting PFN to PA");
+	pfn.x = 0xb;
+	assert(pfn_to_pa(pfn).x == 0xb000,
+	       "pfn_to_pa() not correctly converting PFN to PA");
+	pfn.x = 0;
+	assert(pfn_to_pa(pfn).x == 0,
+	       "pfn_to_pa() not correctly converting PFN to PA");
+
 	return NULL;
 }
 
 /*
  * TODO:
- * bytes_to_pages()
- * phys_to_virt()
- * phys_to_virt_ptr()
  * p*d_to_virt_ptr()
- * virt_to_phys()
- * virt_ptr_to_phys()
- * zero_page()
- * pa_to_pfn()
- * pfn_to_pa()
- * pa_next|prev_page()
  * p*de_present()
  * p*d_index()
  * assign_p*d()
@@ -50,6 +133,10 @@ const char *assert_correct_virtaddr(void)
 const char *test_page(void)
 {
 	const char *ret = assert_correct_virtaddr();
+	if (ret != NULL)
+		return ret;
+
+	ret = assert_page_misc();
 	if (ret != NULL)
 		return ret;
 
