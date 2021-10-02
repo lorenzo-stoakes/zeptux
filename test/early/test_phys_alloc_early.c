7136430fc5db439bff5c94b7652300e7afc21346
Lorenzo Stoakes <lstoakes@gmail.com>
2021-10-02 20:29:55 +0100

    test: mm: add more testing and corect align checks

--- test/early/test_phys_alloc_early.c
+++ test/early/test_phys_alloc_early.c
@@ -29,18 +29,38 @@ const char *test_phys_alloc(void)
 	// lowest with available memory. We test splitting/compacting later.
 	for (int order = max_order; order >= 0; order--) {
 		uint64_t num_free_pages = stats->order[order].num_free_pages;
+		uint64_t num_4k_pages = stats->num_free_4k_pages;
+
 		// Skip cases which would require splitting.
 		if (num_free_pages == 0)
 			continue;
 
 		physaddr_t pa = phys_alloc(order, ALLOC_KERNEL);
-		assert(IS_ALIGNED(pa.x, 1UL << order), "Misaligned PA?");
+
+		assert(IS_ALIGNED(pa.x, 1UL << (order + PAGE_SHIFT)),
+		       "Misaligned PA?");
 
 		assert(stats->order[order].num_free_pages == num_free_pages - 1,
 		       "Stats not updated after alloc?");
+		assert(stats->num_free_4k_pages == num_4k_pages - (1UL << order),
+		       "4 KiB pages stats not updated after alloc?");
 		phys_free(pa);
 		assert(stats->order[order].num_free_pages == num_free_pages,
 		       "Stats not updated after free?");
+	}
+
+	// Now allocate every available 4 KiB page.
+	uint64_t num_order0_pages = stats->order[0].num_free_pages;
+	assert(num_order0_pages > 0, "Not enough order 0 pages to test?");
+
+	uint64_t prev = num_order0_pages;
+	for (uint64_t i = 0; i < num_order0_pages; i++, prev--) {
+		physaddr_t pa = phys_alloc_one();
+		assert(stats->order[0].num_free_pages == prev - 1,
+		       "Allocation not update stats?");
+
+		assert(IS_ALIGNED(pa.x, PAGE_SIZE),
+		       "4 KiB page size not aligned");
 	}
 
 	return NULL;
