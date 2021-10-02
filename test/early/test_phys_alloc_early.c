06249d2c999fb661864ee797636c979e205208cc
Lorenzo Stoakes <lstoakes@gmail.com>
2021-10-02 19:43:36 +0100

    test: mm: check for correct stats then check all non-split/compact orders

--- test/early/test_phys_alloc_early.c
+++ test/early/test_phys_alloc_early.c
@@ -7,27 +7,41 @@ const char *test_phys_alloc(void)
 	spinlock_release(&state->lock);
 	struct phys_alloc_stats *stats = &state->stats;
 
-	int order = MAX_ORDER;
-	for (; order >= 0; order--) {
-		if (stats->order[order].num_free_pages > 0)
+	for (uint8_t order = 0; order < MAX_ORDER; order++) {
+		uint64_t count = list_count(&state->free_lists[order]);
+		assert(count == stats->order[order].num_free_pages,
+		       "Mismatch between stats and list count?");
+	}
+
+	int max_order = MAX_ORDER;
+	for (; max_order >= 0; max_order--) {
+		if (stats->order[max_order].num_free_pages > 0)
 			break;
 	}
 
-	assert(order > 0, "No free memory?");
+	assert(max_order > 0, "No free memory?");
 
 	// We are still in early stage kernel mode, single core and we have only
 	// initialised things up to the point of having a physical allocator, so
 	// we can rely on only us allocating.
 
-	// Try allocating/freeing from the highest available order page.
-	uint64_t num_free_pages = stats->order[order].num_free_pages;
-	physaddr_t pa = phys_alloc(order, ALLOC_KERNEL);
-	assert(IS_ALIGNED(pa.x, 1UL << order), "Misaligned PA?");
-	assert(stats->order[order].num_free_pages == num_free_pages - 1,
-	       "Stats not updated after alloc?");
-	phys_free(pa);
-	assert(stats->order[order].num_free_pages == num_free_pages,
-	       "Stats not updated after free?");
+	// Try allocating/freeing from the highest available order page to the
+	// lowest with available memory. We test splitting/compacting later.
+	for (int order = max_order; order >= 0; order--) {
+		uint64_t num_free_pages = stats->order[order].num_free_pages;
+		// Skip cases which would require splitting.
+		if (num_free_pages == 0)
+			continue;
+
+		physaddr_t pa = phys_alloc(order, ALLOC_KERNEL);
+		assert(IS_ALIGNED(pa.x, 1UL << order), "Misaligned PA?");
+
+		assert(stats->order[order].num_free_pages == num_free_pages - 1,
+		       "Stats not updated after alloc?");
+		phys_free(pa);
+		assert(stats->order[order].num_free_pages == num_free_pages,
+		       "Stats not updated after free?");
+	}
 
 	return NULL;
 }
