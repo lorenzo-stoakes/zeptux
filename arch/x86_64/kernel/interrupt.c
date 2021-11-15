d1f3a3a2fb0b20daf1b3ebde0d7ae91a471bffb3
Lorenzo Stoakes <lstoakes@gmail.com>
2021-11-15 22:49:19 +0000

    interrupt: rename functions for consistency

--- arch/x86_64/kernel/interrupt.c
+++ arch/x86_64/kernel/interrupt.c
@@ -51,30 +51,30 @@ static volatile uint32_t *lapic_reg_ptr(uint64_t offset)
 }
 
 // Read from local APIC register.
-static uint32_t read_lapic_reg(uint64_t offset)
+static uint32_t lapic_read_reg(uint64_t offset)
 {
 	return *lapic_reg_ptr(offset);
 }
 
 // Write to local APIC register.
-static void write_lapic_reg(uint64_t offset, uint32_t val)
+static void lapic_write_reg(uint64_t offset, uint32_t val)
 {
 	volatile uint32_t *ptr = lapic_reg_ptr(offset);
 	*ptr = val;
 }
 
 // Clear error status.
-static void clear_error(void)
+static void lapic_clear_error(void)
 {
 	// Requires 2 writes.
-	write_lapic_reg(APIC_ESR_OFFSET, 0);
-	write_lapic_reg(APIC_ESR_OFFSET, 0);
+	lapic_write_reg(APIC_ESR_OFFSET, 0);
+	lapic_write_reg(APIC_ESR_OFFSET, 0);
 }
 
 // Mark an interrupt cleared.
-static void clear_interrupt(void)
+static void lapic_clear_interrupt(void)
 {
-	write_lapic_reg(APIC_EOI_OFFSET, 0);
+	lapic_write_reg(APIC_EOI_OFFSET, 0);
 }
 
 // Initialise the local APIC.
@@ -82,7 +82,7 @@ static void lapic_init(void)
 {
 	// Retrieve the number of local vector table entries. See intel manual
 	// 3a, figure 10-7.
-	uint32_t ver = read_lapic_reg(APIC_VER_OFFSET);
+	uint32_t ver = lapic_read_reg(APIC_VER_OFFSET);
 	uint32_t max_lvt_entries = (ver & BIT_MASK_BELOW(23)) >> 16;
 	num_lvt_entries = max_lvt_entries + 1;
 
@@ -90,37 +90,37 @@ static void lapic_init(void)
 	// conjunction with the legacy PIC (we've disabled that) and sometimes
 	// for NMIs... but probably not in a multicore system.
 	// TODO: Research this further.
-	write_lapic_reg(APIC_LINT0_OFFSET, APIC_MASK_FLAG);
-	write_lapic_reg(APIC_LINT1_OFFSET, APIC_MASK_FLAG);
+	lapic_write_reg(APIC_LINT0_OFFSET, APIC_MASK_FLAG);
+	lapic_write_reg(APIC_LINT1_OFFSET, APIC_MASK_FLAG);
 
 	// Clear any error status.
-	clear_error();
+	lapic_clear_error();
 	// Clear any residual interrupts.
-	clear_interrupt();
+	lapic_clear_interrupt();
 
 	// Set error interrupt IRQ.
-	write_lapic_reg(APIC_ERROR_OFFSET, APIC_IRQ_0 + APIC_IRQ_ERROR);
+	lapic_write_reg(APIC_ERROR_OFFSET, APIC_IRQ_0 + APIC_IRQ_ERROR);
 
 	// Configure the timer.
-	write_lapic_reg(APIC_TDCR_OFFSET, 0xb); // Divide counts by 1.
-	write_lapic_reg(APIC_TIMER_OFFSET,
+	lapic_write_reg(APIC_TDCR_OFFSET, 0xb); // Divide counts by 1.
+	lapic_write_reg(APIC_TIMER_OFFSET,
 			APIC_PERIODIC_FLAG | (APIC_IRQ_0 + APIC_IRQ_TIMER));
 	// We count down from this.
-	write_lapic_reg(APIC_TICR_OFFSET, APIC_INIT_TIMER_COUNT_VALUE);
+	lapic_write_reg(APIC_TICR_OFFSET, APIC_INIT_TIMER_COUNT_VALUE);
 
 	// Disable performance counter overflow interrupts.
 	if (num_lvt_entries >= 4)
-		write_lapic_reg(APIC_PCINT_OFFSET, APIC_MASK_FLAG);
+		lapic_write_reg(APIC_PCINT_OFFSET, APIC_MASK_FLAG);
 
 	// Enable local APIC (step 1) - specify spurious interrupt.
 	// See https://en.wikipedia.org/wiki/Interrupt#Spurious_interrupts
-	write_lapic_reg(APIC_SPURIOUS_OFFSET,
+	lapic_write_reg(APIC_SPURIOUS_OFFSET,
 			APIC_ENABLE_FLAG | (APIC_IRQ_0 + APIC_IRQ_SPURIOUS));
 	// Enable local APIC (step 2) - Set task priority register which
 	// determines the MINIMUM interrupt priority we want the APIC to
 	// process. By setting to 0, we enable all external interrupts.
 	// NOTE: This enables interrupts on the APIC, not the CPU.
-	write_lapic_reg(APIC_TPR_OFFSET, 0);
+	lapic_write_reg(APIC_TPR_OFFSET, 0);
 }
 
 void interrupt_init(void)
